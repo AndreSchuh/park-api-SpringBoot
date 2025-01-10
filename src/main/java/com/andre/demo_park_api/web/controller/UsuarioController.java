9aa3130c1170e945bdafa4e46cf96216fdf5a419
André <andregabschuh@gmail.com>
2025-01-10 12:32:11 -0300

    Adicionando findById

--- src/main/java/com/andre/demo_park_api/web/controller/UsuarioController.java
+++ src/main/java/com/andre/demo_park_api/web/controller/UsuarioController.java
@@ -5,10 +5,7 @@ import com.andre.demo_park_api.service.UsuarioService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/v1/usuarios")
@@ -22,8 +19,13 @@ public class UsuarioController {
 
     @PostMapping
     public ResponseEntity<Usuario> create(@Valid @RequestBody Usuario usuario){
-
         Usuario user = usuarioService.salvar(usuario);
         return ResponseEntity.status(HttpStatus.CREATED).body(user);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Usuario> findById(@PathVariable Long id){
+        Usuario user = usuarioService.buscarPorId(id);
+        return ResponseEntity.ok().body(user);
     }
 }
